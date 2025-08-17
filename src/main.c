4eb0cdbc3dd109c9f33bbfb813b54e47acc1f123
Michael Huitt <michael.c.huitt@gmail.com>
2025-08-17 02:31:45 -0400

    basic menu navigation implemented

--- src/main.c
+++ src/main.c
@@ -8,31 +8,70 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define WIN_HEIGHT 30
-#define WIN_WIDTH 90
 #define START_X 0
 #define START_Y 0
 
 
 void main(int argc, char *argv[]) {
 	char *path = argv[1];
-	char **arr = populate_entries(path);	
+	char **dir_arr = populate_entries(path);	
 	int x_max, y_max;
+	unsigned int num_files = 0;	
+
+	alphabetize(dir_arr);
+
+	while (dir_arr[num_files] != NULL) {
+		num_files++;	
+	}
 
 	initscr();
 	cbreak();
 	noecho();
-
 	getmaxyx(stdscr, y_max, x_max);
 
 	WINDOW *main_win = newwin(y_max, x_max, START_X, START_Y);
+	keypad(main_win, true);	
 	refresh();	
 	
 	box(main_win, 0, 0);
-	wrefresh(main_win);
+
+	wrefresh(main_win);	
 	
-	getch();
+	int selection;	
+	int highlighted_entry = 0;	
 	
-	free(arr);	
+	while(1) {
+		for (int i = 0; i < num_files || i == y_max; i++) {
+			if (i == highlighted_entry) {
+				wattron(main_win, A_REVERSE);	
+			}	
+			
+			mvwprintw(main_win, START_Y + 1 + i, START_X +1, dir_arr[i]);	
+			wattroff(main_win, A_REVERSE);	
+		}	
+		
+		selection = wgetch(main_win);	
+		
+		switch(selection) {
+			case (KEY_UP):
+				highlighted_entry--;
+				if (highlighted_entry < 0) {
+					highlighted_entry = 0;	
+				}	
+				break;
+			
+			case (KEY_DOWN):
+				highlighted_entry++;
+				if (highlighted_entry >= y_max || highlighted_entry > (num_files - 1)) {
+					highlighted_entry = (num_files - 1);	
+				}
+				break;			
+	
+			default:
+				break;	
+		}	
+	}
+
+	free(dir_arr);	
 	endwin();	
 }
